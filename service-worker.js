2cc94e6ef1802acf83d06c25fc89108b25ffe01a
Ivana2605 <ivanamihailovic26@gmail.com>
2025-06-23 16:26:16 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,37 +1,23 @@
-const CACHE_NAME = 'agritalia-cache-v1';
+﻿const CACHE_NAME = 'agritalia-cache-v1';
 const urlsToCache = [
-    './',
-    './index.html',
-    './app.js',
-    './manifest.json',
-    './logo.jpg',
-    './icon-192.png',
-    './icon-512.png'
+    '/',
+    '/index.html',
+    '/manifest.json',
+    '/logo.png',
+    // dodaj ovde i ostale statične fajlove kao CSS i JS ako ih imaš
 ];
 
 self.addEventListener('install', event => {
     event.waitUntil(
-        caches.open(CACHE_NAME).then(cache => {
-            return cache.addAll(urlsToCache);
-        })
-    );
-});
-
-self.addEventListener('activate', event => {
-    event.waitUntil(
-        caches.keys().then(keys => {
-            return Promise.all(
-                keys.filter(key => key !== CACHE_NAME)
-                    .map(key => caches.delete(key))
-            );
-        })
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(urlsToCache))
     );
 });
 
 self.addEventListener('fetch', event => {
     event.respondWith(
-        caches.match(event.request).then(response => {
-            return response || fetch(event.request);
-        })
+        caches.match(event.request)
+            .then(response => response || fetch(event.request))
     );
 });
+
